ac8c901bb122d50a93289896b28652c9a8f30a69
parmarHimali <himaliparmar13@gmail.com>
2024-09-19 13:24:39 +0530

    removed extra comments

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -3,31 +3,6 @@ import { catchAsyncError } from "./../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/error.js";
 import { Job } from "../models/jobModel.js";
 
-// export const employerGetAllApplications = catchAsyncError(
-//   async (req, res, next) => {
-//     const { role } = req.user;
-//     if (role === "Job Seeker") {
-//       return next(
-//         new ErrorHandler(
-//           "Job seeker is not allowed to access this resource!",
-//           400
-//         )
-//       );
-//     }
-
-//     const { _id } = req.user;
-
-//     // Fetch applications where the employerId matches the logged-in employer
-//     const applications = await Application.find({ "employerId.user": _id })
-//       .populate("applicantId.user", "name email")
-//       .populate("jobId", "title");
-
-//     res.status(200).json({
-//       success: true,
-//       applications,
-//     });
-//   }
-// );
 export const employerGetAllApplications = catchAsyncError(
   async (req, res, next) => {
     const { role } = req.user;
@@ -54,31 +29,6 @@ export const employerGetAllApplications = catchAsyncError(
   }
 );
 
-// export const jobSeekerGetAllApplications = catchAsyncError(
-//   async (req, res, next) => {
-//     const { role } = req.user;
-//     if (role === "Employer") {
-//       return next(
-//         new ErrorHandler(
-//           "Employer is not allowed to access this resource!",
-//           400
-//         )
-//       );
-//     }
-
-//     const { _id } = req.user;
-
-//     // Fetch applications where the applicantId matches the logged-in job seeker
-//     const applications = await Application.find({ "applicantId.user": _id })
-//       .populate("employerId.user", "name email") // Optionally populate employer details
-//       .populate("jobId", "title"); // Optionally populate job details
-
-//     res.status(200).json({
-//       success: true,
-//       applications,
-//     });
-//   }
-// );
 export const jobSeekerGetAllApplications = catchAsyncError(
   async (req, res, next) => {
     const { role } = req.user;
@@ -105,29 +55,6 @@ export const jobSeekerGetAllApplications = catchAsyncError(
   }
 );
 
-// export const jobSeekerDeleteApplication = catchAsyncError(
-//   async (req, res, next) => {
-//     const { role } = req.user;
-//     if (role == "Employer") {
-//       return next(
-//         new ErrorHandler(
-//           "Employer is not allowed to access this resourse!",
-//           400
-//         )
-//       );
-//     }
-//     const { id } = req.params;
-//     const application = await Application.findById(id);
-//     if (!application) {
-//       return next(new ErrorHandler("Oops! application not found.", 404));
-//     }
-//     await application.deleteOne();
-//     res.status(200).json({
-//       success: true,
-//       message: "Application deleted successfully!",
-//     });
-//   }
-// );
 export const jobSeekerDeleteApplication = catchAsyncError(
   async (req, res, next) => {
     const { role } = req.user;
@@ -158,56 +85,6 @@ export const jobSeekerDeleteApplication = catchAsyncError(
   }
 );
 
-// export const postApplication = async (req, res, next) => {
-//   try {
-//     const { jobId, name, email, phone, address, coverLetter } = req.body;
-//     const resume = req.file ? req.file.path : null;
-
-//     // Find the job by ID and populate postedBy (employer information)
-//     const job = await Job.findById(jobId).populate("postedBy");
-
-//     if (!job) {
-//       return next(new ErrorHandler("Job not found", 404));
-//     }
-
-//     const employerId = job.postedBy;
-
-//     if (!employerId) {
-//       return next(new ErrorHandler("Employer not found for this job", 404));
-//     }
-
-//     // Assuming req.user contains the applicant's user ID
-//     const applicantId = req.user._id;
-
-//     // Create the application
-//     const application = new Application({
-//       name,
-//       email,
-//       phone,
-//       address,
-//       coverLetter,
-//       resume,
-//       applicantId: {
-//         user: applicantId,
-//         role: "Job Seeker",
-//       },
-//       employerId: {
-//         user: employerId._id, // This is the postedBy (employerId) from the Job schema
-//         role: "Employer",
-//       },
-//     });
-
-//     await application.save();
-
-//     res.status(201).json({
-//       success: true,
-//       message: "Application submitted successfully",
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return next(new ErrorHandler(error.message, 500));
-//   }
-// };
 export const postApplication = catchAsyncError(async (req, res, next) => {
   try {
     const { name, email, phone, address, coverLetter } = req.body;
